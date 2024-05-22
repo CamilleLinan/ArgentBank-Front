b59d481b3484cfea6db75aa626c53beebd1dfd06
Camille Linan <camille.linan@hotmail.com>
2024-05-22 22:46:19 +0200

    add footer component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Header from "../../components/Layout/Header/Header";
 import Banner from "../../components/Home/Banner/Banner";
 import Features from "../../components/Home/Features/Features";
+import Footer from "../../components/Layout/Footer/Footer";
 
 const Home: FC = () => {
   return (
@@ -11,9 +12,7 @@ const Home: FC = () => {
         <Banner />
         <Features />
       </main>
-      <footer className="footer">
-        <p className="footer-text">Copyright 2020 Argent Bank</p>
-      </footer>
+      <Footer />
     </>
   );
 };

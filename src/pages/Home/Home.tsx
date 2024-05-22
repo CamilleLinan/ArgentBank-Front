60f65fbb00f804fc9eaf81f5ffc053e547f9b5a0
Camille Linan <camille.linan@hotmail.com>
2024-05-22 22:41:02 +0200

    add features component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,7 @@
 import { FC } from "react";
-import iconChat from "../../assets/icon-chat.png";
-import iconMoney from "../../assets/icon-money.png";
-import iconSecurity from "../../assets/icon-security.png";
 import Header from "../../components/Layout/Header/Header";
 import Banner from "../../components/Home/Banner/Banner";
+import Features from "../../components/Home/Features/Features";
 
 const Home: FC = () => {
   return (
@@ -11,38 +9,7 @@ const Home: FC = () => {
       <Header />
       <main>
         <Banner />
-        <section className="features">
-          <h2 className="sr-only">Features</h2>
-          <div className="feature-item">
-            <img src={iconChat} alt="Chat Icon" className="feature-icon" />
-            <h3 className="feature-item-title">You are our #1 priority</h3>
-            <p>
-              Need to talk to a representative? You can get in touch through our
-              24/7 chat or through a phone call in less than 5 minutes.
-            </p>
-          </div>
-          <div className="feature-item">
-            <img src={iconMoney} alt="Money Icon" className="feature-icon" />
-            <h3 className="feature-item-title">
-              More savings means higher rates
-            </h3>
-            <p>
-              The more you save with us, the higher your interest rate will be!
-            </p>
-          </div>
-          <div className="feature-item">
-            <img
-              src={iconSecurity}
-              alt="Security Icon"
-              className="feature-icon"
-            />
-            <h3 className="feature-item-title">Security you can trust</h3>
-            <p>
-              We use top of the line encryption to make sure your data and money
-              is always safe.
-            </p>
-          </div>
-        </section>
+        <Features />
       </main>
       <footer className="footer">
         <p className="footer-text">Copyright 2020 Argent Bank</p>

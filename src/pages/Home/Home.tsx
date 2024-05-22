61452c76f4a0695784493681d78f1a4f14cc3037
Camille Linan <camille.linan@hotmail.com>
2024-05-22 21:59:09 +0200

    add header component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,13 @@
 import { FC } from "react";
-import logo from "../../assets/argentBankLogo.png";
 import iconChat from "../../assets/icon-chat.png";
 import iconMoney from "../../assets/icon-money.png";
 import iconSecurity from "../../assets/icon-security.png";
+import Header from "../../components/Layout/Header/Header";
 
 const Home: FC = () => {
   return (
     <>
-      <nav className="main-nav">
-        <a className="main-nav-logo" href="./index.html">
-          <img
-            className="main-nav-logo-image"
-            src={logo}
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
-        </a>
-        <div>
-          <a className="main-nav-item" href="./sign-in.html">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </a>
-        </div>
-      </nav>
+      <Header />
       <main>
         <div className="hero">
           <section className="hero-content">

c396d2d2bef8e76132262bfe117116077add804b
Camille Linan <camille.linan@hotmail.com>
2024-05-22 20:58:23 +0200

    init home page

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+
+const IndexRoutes:FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default IndexRoutes;
